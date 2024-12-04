ec27bef866204b6afb4471ab3a8442c2e2d541b8
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2024-12-04 16:17:27 +0100

    feat: finish palindroma

--- palidroma/script.js
+++ palidroma/script.js
@@ -1,2 +1,21 @@
 // Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+const word = prompt("Inserisci una parola");
+function reversedWord(word) {
+    let wordReversed = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        wordReversed = wordReversed + word[i];
+    }
+    return wordReversed;
+}
+
+// Creare una funzione per capire se la parola insrita è palindroma
+function isPalindrome(word) {
+    let wordReversed = reversedWord(word);
+    if (word === reversedWord(word)) {
+        return true
+        } else {
+            return false
+        }
+    }
+    
+alert(isPalindrome(word));
